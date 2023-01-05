638f7ab2fc5da87846ca167bc6c901723ef92435
lumoshift <dario.sangiovanni@gmail.com>
2023-01-05 23:37:41 +0100

    Initial Maven setup

---
+++ src/main/java/com/tenut/asymmetric-key-generator/App.java
@@ -0,0 +1,13 @@
+package com.tenut.asymmetric-key-generator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
